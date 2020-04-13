9134dcf4afc2e5bbe58a4e0590e169c0b38f1679
Yana Petrova <janavpetrova@gmail.com>
2020-04-13 11:01:47 +0300

    fix codestyle

--- apps/web/controllers/recipes/index.rb
+++ apps/web/controllers/recipes/index.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 module Web
   module Controllers
     module Recipes
       class Index
         include Web::Action
 
-        def call(params)
-        end
+        def call(params); end
       end
     end
   end
