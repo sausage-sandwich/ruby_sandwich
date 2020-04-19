13778cb7a117649e3cedcf9d525e8bef7be5ef9d
Yana Petrova <janavpetrova@gmail.com>
2020-04-19 21:51:11 +0300

    save

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 resources :recipes, only: %i[index show new create edit update]
+resources :shopping_lists, only: %i[index show new create edit update]
 
 root to: 'recipes#index'
