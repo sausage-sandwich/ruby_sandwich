6d90a36235ffa0e13ff5c372e17c6cbbba6f0eb8
Alex Klyanchin <jetblackmeerkat@gmail.com>
2020-04-21 22:37:39 +0300

    Use nutrition facts in the ingredient

--- spec/sandwich/entities/ingredient_spec.rb
+++ spec/sandwich/entities/ingredient_spec.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe Ingredient, type: :entity do
-  describe 'calories' do
-    subject(:calories) { ingredient.calories }
+  describe 'nutrition_facts' do
+    subject(:nutrition_facts) { ingredient.nutrition_facts }
 
     let(:ingredient) do
       described_class.new(carbohydrates_mg: 2000, fat_mg: 2000, protein_mg: 2000)
     end
 
     it 'calculates calories from macronutrients' do
-      expect(calories).to eq 34
+      expect(nutrition_facts).to have_attributes(
+        carbohydrates_mg: 2000,
+        fat_mg: 2000,
+        protein_mg: 2000,
+        calories: 34
+      )
     end
   end
 end
