9a457a07b19475a463817ab81e947aaf51183025
Yana Petrova <janavpetrova@gmail.com>
2020-04-25 14:04:19 +0300

    Add signing up (#33)

---
+++ lib/sandwich/interactors/create_user.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'hanami/interactor'
+
+class CreateUser
+  include Hanami::Interactor
+
+  expose :user
+
+  def call(user_params)
+    @user = user_repo.create(secured_params(user_params))
+  end
+
+  private
+
+  def valid?(params)
+    !email_exists?(params.fetch(:email))
+  end
+
+  def secured_params(params)
+    password = params.fetch(:password)
+    password_digest = BCrypt::Password.create(password)
+
+    params.except(:password).merge(password_digest: password_digest)
+  end
+
+  def user_repo
+    @user_repo ||= UserRepository.new
+  end
+
+  def email_exists?(email)
+    !user_repo.find_by_email(email).nil?
+  end
+end
