619fa59dd597f7a633069df6dad140b63bc7a8f3
Yana Petrova <janavpetrova@gmail.com>
2020-05-02 20:12:41 +0300

    fix syntax error in template and add test (#56)
    https://rollbar.com/sausage-sandwich/sausage-sandwich/items/4/

--- spec/web/views/recipes/show_spec.rb
+++ spec/web/views/recipes/show_spec.rb
@@ -1,13 +1,38 @@
 # frozen_string_literal: true
 
 RSpec.describe Web::Views::Recipes::Show, type: :view do
-  let(:exposures) { Hash[format: :html] }
+  let(:exposures) { { recipe: recipe, format: :html } }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/recipes/show.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
+  let(:recipe) { recipe_repo.all_with_ingredients.first }
+  let(:ingredient) do
+    ingredient_repo = IngredientRepository.new
+    ingredient_repo.create(title: 'carrot', carbohydrates_mg: 3000, fat_mg: 1000, protein_mg: 2000)
+  end
+  let(:recipe_repo) { RecipeRepository.new }
+
+  before do
+    recipe_repo.create_with_recipe_ingredients(
+      title: 'title',
+      body: 'body',
+      recipe_ingredients: [
+        {
+          ingredient_id: ingredient.id,
+          ingredient: ingredient,
+          quantity: 100,
+          unit: 'g'
+        }
+      ]
+    )
+  end
 
   it 'exposes #format' do
     expect(view.format).to eq exposures.fetch(:format)
+  end
+
+  it 'renders view' do
+    expect(rendered).to be
   end
 
   it 'presents nutrient in grams' do
