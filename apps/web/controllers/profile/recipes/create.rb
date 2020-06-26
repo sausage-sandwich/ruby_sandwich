3c0896213b4860cf4fd07f61a4d502c6d427e8f1
Yana Petrova <janavpetrova@gmail.com>
2020-06-26 12:10:45 +0300

    simplify code

--- apps/web/controllers/profile/recipes/create.rb
+++ apps/web/controllers/profile/recipes/create.rb
@@ -31,35 +31,18 @@ module Web
           end
 
           def call(params)
-            if params.valid?
-              result = create_recipe.call(params[:recipe].merge(user_id: current_user.id))
-              @recipe = result.recipe
+            return unless params.valid?
 
-              redirect_to routes.profile_recipe_path(id: @recipe.id) if result.success?
-            else
-              @recipe = build_recipe(params[:recipe])
-            end
+            result = create_recipe.call(params[:recipe].merge(user_id: current_user.id))
+            @recipe = result.recipe
+
+            redirect_to routes.profile_recipe_path(id: @recipe.id) if result.success?
           end
 
           private
 
           def create_recipe
             CreateRecipe.new
-          end
-
-          def build_recipe(recipe_params)
-            recipe_ingredients = recipe_params[:recipe_ingredients].map do |recipe_ingredient|
-              RecipeIngredient.new(
-                unit: recipe_ingredient.fetch(:unit),
-                quantity: recipe_ingredient.fetch(:quantity),
-                ingredient: Ingredient.new(title: recipe_ingredient.fetch(:title))
-              )
-            end
-
-            result_params = recipe_params.slice(:title, :body, :image).
-              merge(recipe_ingredients: recipe_ingredients)
-
-            Recipe.new(result_params)
           end
         end
       end
