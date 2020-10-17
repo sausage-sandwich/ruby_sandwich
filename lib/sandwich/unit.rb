0bb25349270bacc1a6d4973c98cfb0cd16eaf247
Yana Petrova <janavpetrova@gmail.com>
2020-10-17 18:40:11 +0300

    toggle imperial/metric units

--- lib/sandwich/unit.rb
+++ lib/sandwich/unit.rb
@@ -5,7 +5,7 @@ class Unit
     ml l gallon quart pint cup fluid_ounce
     tablespoon teaspoon glass handful pinch pieces].freeze
 
-  CONVERSION_RATE = {
+  TO_METRIC_CONVERSION_RATE = {
     gallon: { quantity: 4546.09, unit: :ml },
     quart: { quantity: 1136.52, unit: :ml },
     pint: { quantity: 568.261, unit: :ml },
@@ -16,13 +16,19 @@ class Unit
   TO_GRAMS_CONVERSION_RATE = {
     g: 1,
     kg: 1000,
-    pound: CONVERSION_RATE.fetch(:pound).fetch(:quantity),
-    ounce: CONVERSION_RATE.fetch(:ounce).fetch(:quantity)
+    pound: TO_METRIC_CONVERSION_RATE.fetch(:pound).fetch(:quantity),
+    ounce: TO_METRIC_CONVERSION_RATE.fetch(:ounce).fetch(:quantity)
   }.freeze
   GREATER_UNITS = {
     g: :kg,
     ml: :l
   }.freeze
+
+  TO_METRIC_CONVERSION_RATE.each do |(measure, rate)|
+    define_method "#{measure}_in_#{rate.fetch(:unit)}" do
+      TO_METRIC_CONVERSION_RATE.fetch(measure).fetch(:quantity)
+    end
+  end
 
   ConversionError = Class.new(StandardError)
 
@@ -30,11 +36,11 @@ class Unit
 
   def initialize(quantity, unit)
     @quantity = BigDecimal(quantity)
-    @unit = unit
+    @unit = unit.to_sym
   end
 
   def convert_to_metric
-    metric = CONVERSION_RATE[unit.to_sym]
+    metric = TO_METRIC_CONVERSION_RATE[unit.to_sym]
     return self unless metric
 
     new_quantity = quantity * metric[:quantity]
@@ -42,6 +48,38 @@ class Unit
       Unit.new(new_quantity / 1000, GREATER_UNITS[metric[:unit]])
     else
       Unit.new(new_quantity, metric[:unit])
+    end
+  end
+
+  def convert_to_imperial
+    return self if TO_METRIC_CONVERSION_RATE.keys.include?(unit)
+
+    case unit
+    when :kg
+      Unit.new((quantity * 1000 / pound_in_g), :pound)
+    when :g
+      if quantity < pound_in_g
+        Unit.new((quantity / ounce_in_g), :ounce)
+      else
+        Unit.new((quantity / pound_in_g), :pound)
+      end
+    when :l
+      in_ml = quantity * 1000
+      if in_ml < gallon_in_ml
+        Unit.new((in_ml / pint_in_ml), :pint)
+      else
+        Unit.new((in_ml / gallon_in_ml), :gallon)
+      end
+    when :ml
+      if quantity >= 1000
+        Unit.new(quantity / 1000, :l).convert_to_imperial
+      elsif quantity > pint_in_ml
+        Unit.new((quantity / pint_in_ml), :pint)
+      else
+        Unit.new((quantity / fluid_ounce_in_ml), :fluid_ounce)
+      end
+    else
+      self
     end
   end
 
--- spec/sandwich/unit_spec.rb
+++ spec/sandwich/unit_spec.rb
@@ -28,6 +28,122 @@ RSpec.describe Unit do
     end
   end
 
+  describe '#convert_to_imperial' do
+    subject(:in_imperial) { instance.convert_to_imperial }
+
+    shared_examples 'imperial unit' do |unit|
+      subject(:in_imperial) { instance.convert_to_imperial }
+
+      let(:instance) { described_class.new(BigDecimal(1), unit) }
+
+      it { expect(in_imperial).to eq instance }
+    end
+
+    described_class::TO_METRIC_CONVERSION_RATE.keys.each do |unit|
+      it_behaves_like 'imperial unit', unit
+    end
+
+    context 'when unit is in kg' do
+      let(:instance) { described_class.new(1, :kg) }
+
+      it do
+        expect(in_imperial).to have_attributes(
+          quantity: be_within(0.01).of(BigDecimal('2.2')),
+          unit: :pound
+        )
+      end
+    end
+
+    context 'when unit is in g' do
+      let(:instance) { described_class.new(quantity, :g) }
+
+      context 'when quantity is less then 1 pound' do
+        let(:quantity) { 453 }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('15.97')),
+            unit: :ounce
+          )
+        end
+      end
+
+      context 'when quantity is more then 1 pound' do
+        let(:quantity) { 454 }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('1')),
+            unit: :pound
+          )
+        end
+      end
+    end
+
+    context 'when unit is in l' do
+      let(:instance) { described_class.new(quantity, :l) }
+
+      context 'when quantity is less then 1 gallon' do
+        let(:quantity) { BigDecimal('4.546') }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('7.99')),
+            unit: :pint
+          )
+        end
+      end
+
+      context 'when quantity is more then 1 gallon' do
+        let(:quantity) { BigDecimal('4.547') }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('1')),
+            unit: :gallon
+          )
+        end
+      end
+    end
+
+    context 'when unit is in ml' do
+      let(:instance) { described_class.new(quantity, :ml) }
+
+      context 'when quantity is more or equal then 1 l' do
+        let(:quantity) { BigDecimal(1001) }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('1.76')),
+            unit: :pint
+          )
+        end
+      end
+
+      context 'when quantity is between pint and liter' do
+        let(:quantity) { BigDecimal(569) }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('1')),
+            unit: :pint
+          )
+        end
+      end
+
+      context 'when quantity is less then pint' do
+        let(:quantity) { BigDecimal(567) }
+
+        it do
+          expect(in_imperial).to have_attributes(
+            quantity: be_within(0.01).of(BigDecimal('19.95')),
+            unit: :fluid_ounce
+          )
+        end
+      end
+    end
+  end
+
   describe '#humanize' do
     subject(:humanized_unit) { unit.humanize }
 
