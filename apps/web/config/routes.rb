bb7264c5097b46389bada61e956f86da135446c6
Alex Klyanchin <jetblackmeerkat@gmail.com>
2020-10-29 23:59:28 +0300

    Salads generator
    Generator — done! Controller — in progress.
    
    Closes #84.

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,6 +3,7 @@
 resources :recipes, only: %i[index show]
 resources :users, only: %i[new create]
 resource :session, only: %i[new create destroy]
+resources :salads, only: %i[new create]
 
 root to: 'recipes#index'
 
---
+++ spec/sandwich/interactors/generate_salad_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+RSpec.describe GenerateSalad do
+  subject(:generate_salad) { described_class.new.call }
+
+  let(:available_ingredients) { described_class::INGREDIENTS.values.flatten }
+
+  it { expect(generate_salad).to be_success }
+  it 'generates salad' do
+    expect(available_ingredients).to include(*generate_salad.ingredients)
+  end
+
+  context 'with selected categories' do
+    subject(:generate_salad) { described_class.new.call(categories: categories) }
+
+    let(:categories) { described_class::INGREDIENTS.keys.sample(2) }
+    let(:available_ingredients) { described_class::INGREDIENTS.values_at(*categories).flatten }
+
+    it 'construct within selected categories' do
+      expect(available_ingredients).to include(*generate_salad.ingredients)
+    end
+  end
+
+  context 'with no categories' do
+    subject(:generate_salad) { described_class.new.call(categories: categories) }
+
+    let(:categories) { [] }
+
+    it 'construct within selected categories' do
+      expect(generate_salad.ingredients).to eq []
+    end
+  end
+end
