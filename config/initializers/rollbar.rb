d6606e8fa01e780ef28afa60dadff79720607a49
Alex Klyanchin <jetblackmeerkat@gmail.com>
2020-10-31 11:26:48 +0300

    Fix typo

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,5 +3,5 @@ require 'rollbar'
 Rollbar.configure do |config|
   config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN')
   config.environment = Hanami.env
-  config.enabled = !%w[development test].include(Hanami.env)
+  config.enabled = !%w[development test].include?(Hanami.env)
 end
