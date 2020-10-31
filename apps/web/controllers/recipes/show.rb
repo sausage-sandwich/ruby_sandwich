7079d8093791f29fb6902f271f99cc458a6c6d3a
Alex Klyanchin <jetblackmeerkat@gmail.com>
2020-10-31 23:48:38 +0300

    Add basic code to scale recipe (#89)
    Closes #87

--- apps/web/controllers/recipes/show.rb
+++ apps/web/controllers/recipes/show.rb
@@ -8,10 +8,17 @@ module Web
 
         expose :recipe
 
+        params do
+          optional(:recipe).schema do
+            optional(:scale).maybe(:float?)
+          end
+        end
+
         def call(params)
           repo = RecipeRepository.new
 
-          @recipe = repo.find_with_ingredients(params[:id])
+          recipe = repo.find_with_ingredients(params[:id])
+          @recipe = ScaledRecipe.new(recipe, params.dig(:recipe, :scale) || 1)
         end
       end
     end
