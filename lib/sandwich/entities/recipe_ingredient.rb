e7cd9057765a8343959428c84bb74bc2c5811f22
Yana Petrova <janavpetrova@gmail.com>
2020-11-07 17:33:59 +0300

    restore calculating nutrition info
    make recipe ingredients nutrition info fields nullable
    add links to calculating recipe nutrition info
    cleanup nutrition info calculating page

--- lib/sandwich/entities/recipe_ingredient.rb
+++ lib/sandwich/entities/recipe_ingredient.rb
@@ -2,10 +2,26 @@
 
 class RecipeIngredient < Hanami::Entity
   def nutrition_facts_per_quantity
-    ingredient.nutrition_facts * (quantity_in_grams / 100)
+    nutrition_facts * (quantity_in_grams / 100)
+  end
+
+  def nutrition_facts
+    build_nutrition_facts || ingredient.nutrition_facts
   end
 
   def quantity_in_grams
     Unit.new(quantity, unit).convert_to_grams.quantity
+  end
+
+  private
+
+  def build_nutrition_facts
+    return unless carbohydrates_mg && fat_mg && protein_mg
+
+    NutritionFacts.new(
+      carbohydrates_mg: carbohydrates_mg,
+      fat_mg: fat_mg,
+      protein_mg: protein_mg
+    )
   end
 end
