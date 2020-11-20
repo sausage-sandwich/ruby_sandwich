f2e8640b599c0f410a9bfbd00fcd84ac9645d323
Yana Petrova <janavpetrova@gmail.com>
2020-11-20 20:47:28 +0300

    improve ingredients admin view
    show PFC

--- apps/admin/views/ingredients/index.rb
+++ apps/admin/views/ingredients/index.rb
@@ -7,6 +7,12 @@ module Admin
     module Ingredients
       class Index
         include Admin::View
+
+        def display_g(value)
+          return unless value
+
+          (value / 1000).to_f
+        end
       end
     end
   end
